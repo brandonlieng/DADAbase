cad6df2a4ee6c85d007753bc8c417e4be2ed7874
brandonlieng <brandonlieng@Brandons-MacBook-Pro.local>
2017-10-02 16:59:38 -0400

    .

--- DADAbaseCreate.sql
+++ DADAbaseCreate.sql
@@ -1,39 +1,26 @@
 /*CREATE DATABASE DADAbase;
 
 USE DADAbase;
-
-
-archivedSeqs    holds an archive of sequence variants from previous data
-incoming        holds the incoming data to be split and analyzed using DADAbase
-newSeqs         holds sequence variants that do not show up in existingSeqs
-oldSeqs      holds sequence variants that show up in existingSeqs
 */
-CREATE TABLE archivedSeqs(
-    accessionNum INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE sequence(
+    accessNum INT NOT NULL AUTO_INCREMENT,
     sequence VARCHAR(400),
     taxonomy VARCHAR(400),
-    taxoKeys VARCHAR(100),
-    groupNum INT,
+    taxoMethod VARCHAR(100),
     runNum INT,
-    PRIMARY KEY(accessionNum)
-);
-
-CREATE TABLE knownSeqs(
-    sequence VARCHAR(400)
-);
-
-CREATE TABLE incoming(
-    sequence VARCHAR(400),
-    taxonomy VARCHAR(400),
-    taxoKeys VARCHAR(100),
     groupNum INT,
-    runNum INT
+    PRIMARY KEY(accessNum)
 );
 
-CREATE TABLE entryMetadata(
+CREATE TABLE runNum(
+    accessNum INT,
+    groupNum INT
+);
+
+CREATE TABLE entry(
     runNum INT,
+    entryDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     platform varchar(100),
     operator varchar(100),
-    entryDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY(runNum)
 );

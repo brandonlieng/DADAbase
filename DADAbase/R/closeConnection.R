d5ebb0ea4a92f15b83e3055c922c4426add783c3
brandonlieng <blieng@uwo.ca>
2017-12-04 16:58:24 -0500

    Remove remnants of old DADAbase schema from closeConnection.R

--- DADAbase/R/closeConnection.R
+++ DADAbase/R/closeConnection.R
@@ -1,14 +1,8 @@
 #' Close the connection to the SQL database
 #'
 #' @description
-#' Purges the incoming and newSeqs tables to reset DADAbase to a state where it can recieve a new dataset. After purging, the connection to the MySQL server is closed.
+#' Connection to the SQLite server is closed.
 #' @export
-DADAbase.closeConnection <- function() {
-    query <- paste("TRUNCATE knownSeqs;")
-    dbGetQuery(ch, query)
-
-    query <- paste("TRUNCATE incoming;")
-    dbGetQuery(ch, query)
-
+dadabase.closeconnection <- function() {
     dbDisconnect(ch)
 }
